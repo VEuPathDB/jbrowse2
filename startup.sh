ee5ebc02a58c811b7b7fa527b188ef5d6688c193
Ryan Doherty <tech@conical.org>
2023-09-27 23:22:06 -0400

    Put config.json in the right location and edit track paths; also fix npx command

--- startup.sh
+++ startup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env sh
 
+# clean up jbrowse2 raw installation
 cd jbrowse2
 rm -rf test_data
+
+# link to mounted track data
 ln -s $SERVICE_FILES_MOUNT data
+
+# need to copy the config.json file and fix paths
+sed 's|uri": "|uri": "data/|' data/config.json > config.json
+
 echo "Starting up node server on port $JBROWSE2_SERVER_PORT"
-npx serve -S -p $JBROWSE2_SERVER_PORT
+npx serve -S -p $JBROWSE2_SERVER_PORT .

2c8c839a2b44cb1dee65c07eb6af346391ce7e7f
Ryan Doherty <tech@conical.org>
2023-10-03 22:30:58 -0400

    Add code to check config.json occasionally and refresh in the container on modification

--- startup.sh
+++ startup.sh
@@ -14,8 +14,24 @@ fi
 rm -f data
 ln -s $SERVICE_FILES_MOUNT data
 
-# need to copy the config.json file and fix paths
-sed 's|uri": "|uri": "data/|' data/config.json > config.json
+# need to copy the config.json file and fix paths; monitor this file and refresh if it is modified
+runConfigRefresherThread() {
+  while true; do
+    timestamp=`date`
+    echo -n "$timestamp Checking config.json... "
+    newModDate=`stat -c %y data/config.json`
+    if [ "$newModDate" != "$modDate" ]; then
+      sed 's|uri": "|uri": "data/|' data/config.json > config.json.new
+      mv config.json.new config.json # more atomic than sed
+      modDate=$newModDate
+      echo "modified.  Updated."
+    else
+      echo "unmodified."
+    fi
+    sleep $CONFIG_JSON_CHECK_INTERVAL_SECS
+  done
+}
+runConfigRefresherThread &
 
 echo "Starting up node server on port $JBROWSE2_SERVER_PORT"
 npx serve -S -p $JBROWSE2_SERVER_PORT .
